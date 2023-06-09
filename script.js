54e73f4580b078e1a9914ad3640e11a3e0efcbf1
SimoneRicco <smnrcc0@gmail.com>
2023-06-09 17:18:09 +0200

    add cards

--- script.js
+++ script.js
@@ -8,17 +8,9 @@ const app = Vue.createApp({
         request() {
           axios.get('http://localhost/php-dischi-json/server.php').then(response => this.myArr = response.data);
         },
-        printer(){
-          console.log("lunghezza "+this.myArr.length);
-          for (let item in this.myArr) {
-            console.log(item.titolo);
-            console.log("ciao");
-          }
-        }
       },
       created() {
         this.request();
-        this.printer();
       },
 });
 
--- server.php
+++ server.php
@@ -1,26 +1,5 @@
 <?php
 
 header("Content-Type: application/json");
-// $data = file_get_contents('database.json');
-$list = '[
-    {
-        "immagine": "img",
-        "titolo": "tt",
-        "cantante": "cc",
-        "anno": 1234
-    },
-    {
-        "immagine": "img",
-        "titolo": "aba",
-        "cantante": "cc",
-        "anno": 1234
-    },
-    {
-        "immagine": "img",
-        "titolo": "nun",
-        "cantante": "cc",
-        "anno": 1234
-    }
-]';
-$data = json_encode($list);
+$data = file_get_contents('database.json');
 echo $data;
